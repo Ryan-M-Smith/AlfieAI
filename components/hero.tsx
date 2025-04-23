18e27b3da78e1a297925e3cf5e45b6c2712335d6
Ryan Smith <rysmith2113@gmail.com>
2025-04-23 16:48:01 -0400

    Add the logo and masthead to the hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,9 @@
 
 "use client";
 
+import Image from "next/image";
 import { JSX } from "react";
+import Link from "next/link";
 import { useTime, useTransform, useMotionTemplate, motion } from "framer-motion";
 
 export default function Hero(): JSX.Element {
@@ -24,23 +26,59 @@ export default function Hero(): JSX.Element {
 	)`;
 
 	return (
-		<div className="flex justify-center items-center mx-5 h-screen dark:text-white text-black text-center pb-20">
-			<div className="flex flex-col justify-center items-center rounded-lg">
-				<div className="relative">
-					<div className="dark:bg-default-50 bg-blue-200 dark:text-gray-200 p-10 rounded-lg">
-						<h1 className="text-5xl font-bold mb-4">
-							Welcome to AlfieAI
-						</h1>
+		<div className="flex justify-center items-center mx-5 h-screen dark:text-white text-black text-center pb-20 rounded-[16px]">
+			<div className="relative">
+				<div className={`
+					flex flex-col justify-center items-center dark:bg-default-50 bg-blue-200
+					dark:text-gray-200 p-10 rounded-lg
+				`}>
+					<Image className="rounded-[16px]" src="/logo.png" alt="AlfieAI logo" width={256} height={256}/>
+					
+					<h1 className="text-5xl font-bold mt-8">
+						Say hello to AlfieAI 👋
+					</h1>
 
-						<p className="text-2xl mb-8">
-							The AI expert on Juniata College. No eagle flies without AI! 🦅
-						</p>
+					<p className="text-2xl mt-4">
+						The AI expert on Juniata College. No eagle flies without AI! 🦅
+					</p>
+
+					<div className="flex gap-x-1 text-sm italic text-gray-500 mt-8 mb-0">
+						<span> Copyright © 2025 Ryan Smith & Adithya Kommi. Powered by </span>
+						<Link
+							className="underline"
+							href="https://nextjs.org"
+							rel="noopener noreferrer"
+							target="_blank"
+							passHref
+						>
+							Next.js,
+						</Link>
+						<Link
+							className="underline"
+							href="https://gemini.google.com/app"
+							rel="noopener noreferrer"
+							target="_blank"
+							passHref
+						>
+							Gemini,
+						</Link>
+						<span> and </span>
+						<Link
+							className="underline"
+							href="https://vercel.com"
+							rel="noopener noreferrer"
+							target="_blank"
+							passHref
+						>
+							Vercel.
+						</Link>
 					</div>
-					<motion.div
-						className="absolute -inset-[10px] -z-10 rounded-lg shadow-lg dark:shadow-blue-300 shadow-gray-700"
-						style={{ background: bg }}
-					/>
 				</div>
+
+				<motion.div
+					className="absolute -inset-[10px] -z-10 rounded-[16px] shadow-lg dark:shadow-blue-300 shadow-gray-700"
+					style={{ background: bg }}
+				/>
 			</div>
 		</div>
 	);
