3d3eb7099e8a546e08d2351cc45d89713fdc7162
Ryan Smith <rysmith2113@gmail.com>
2025-04-23 14:27:58 -0400

    Create a loading view

--- components/chat-bubble.tsx
+++ components/chat-bubble.tsx
@@ -4,16 +4,18 @@
 // Copyright (c) 2025 Ryan Smith, Adithya Kommi
 //
 
-import { JSX, ReactNode } from "react"; 
+import { JSX, ReactNode } from "react";
+import { Spinner } from "@heroui/spinner";
 import { TbTriangleInvertedFilled } from "react-icons/tb";
 
 interface ChatBubbleProps {
 	className?: string;
-	children?: ReactNode;
-	align?: "left" | "right";
+	children?: 	ReactNode;
+	align?: 	"left" | "right";
+	isLoading?: boolean;
 }
 
-export default function ChatBubble({ className, children, align }: ChatBubbleProps): JSX.Element {
+export default function ChatBubble({ className, children, align, isLoading }: ChatBubbleProps): JSX.Element {
 	const justify = align === "left"? "justify-start" : "justify-end";
 	const items = align === "left"? "items-start" : "items-end";
 	const side = align === "left"? "-left-[1.5]" : "right-0";
@@ -22,13 +24,22 @@ export default function ChatBubble({ className, children, align }: ChatBubblePro
 		<div className={`${className} ${justify} flex w-full px-2 sm:px-4 my-1`}>
 			<div className={`${items} flex flex-col relative max-w-[85%] sm:max-w-[75%]`}>
 				{ /* Bubble */ }
-				<div className="rounded-lg bg-default-100 px-3 sm:px-5 py-2 w-full break-words whitespace-normal overflow-hidden">
-					{children}
+				<div className="rounded-lg dark:bg-default-100 bg-blue-200 px-3 sm:px-5 py-2 w-full break-words whitespace-normal overflow-hidden">
+					{
+						isLoading? (
+							<Spinner
+								className="flex justify-center items-center"
+								label="AlfieAI is thinking"
+								color="primary"
+								variant="wave"
+							/>
+						) : children
+					}
 				</div>
 
 				{ /* Tail */}
 				<TbTriangleInvertedFilled
-					className={`relative top-[-13px] ${side} text-default-100 rotate-[3deg]`}
+					className={`relative top-[-13px] ${side} dark:text-default-100 text-blue-200 rotate-[3deg]`}
 					size={30}
 				/>
 			</div>
