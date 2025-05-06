a5e980218ae0117b13316a77dd28616a4f9f2de7
Ryan Smith <rysmith2113@gmail.com>
2025-05-06 16:23:30 -0400

    Add a way to embed queries

---
+++ lib/ai.ts
@@ -0,0 +1,20 @@
+//
+// Filename: ai.ts
+// Description: Machine learning and AI related functions
+// Copyright (c) 2025 Ryan Smith, Adithya Kommi
+//
+
+import { pipeline } from "@xenova/transformers"
+
+export async function embed(text: string) {
+	// Load the embedding pipeline with a sentence transformer model
+	const extractor = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2");
+
+	// Get embedding for a sentence
+	const output = await extractor(text, {
+		pooling: "mean",
+		normalize: true
+	});
+
+	return Array.from(output.data);
+}
