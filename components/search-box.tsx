0ff28da8b9f56b9da2b7fe75df5800a735ac4edc
Ryan Smith <rysmith2113@gmail.com>
2025-06-04 23:54:29 -0400

    Add animated placeholder text for the search box

--- components/search-box.tsx
+++ components/search-box.tsx
@@ -8,11 +8,15 @@
 
 import { Button } from "@heroui/button";
 import { FaArrowCircleUp } from "react-icons/fa";
+import { gsap } from "gsap";
 import { IoSearch } from "react-icons/io5";
-import { Dispatch, JSX, KeyboardEvent, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, JSX, KeyboardEvent, SetStateAction, useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Textarea } from "@heroui/input";
+
+import placeholders from "@/lib/placeholders";
+import { delay } from "framer-motion";
 
 interface SearchBoxProps {
 	setIsSearching?: Dispatch<SetStateAction<boolean>>;
@@ -24,31 +28,132 @@ export default function SearchBox({ setIsSearching }: SearchBoxProps): JSX.Eleme
 	const [query, setQuery] = useState<string>("");
 	const [canSend, setCanSend] = useState<boolean>(false);
 	const [placeholder, setPlaceholder] = useState<string>("");
-	
-	const placeholders = [
-		"Graduates with a masters degree in Computer Science",
-		"Math teachers near State College",
-		"Biology students graudating in 2027",
-		"Engineers who work in big tech",
-		"Scientists using machine learning in their research",
-	];
+	const [isCleared, setIsCleared] = useState(false);
 
-	useEffect(() => {
-		const index = Math.floor(Math.random() * placeholders.length);
-		setPlaceholder(placeholders[index]);
+	/**
+	 * Reset GSAP animations when the document visibility changes to avoid rendering issues
+	 */
+	const onChangeVisibility = useCallback(() => {
+		if (document.visibilityState !== "visible") {
+			setPlaceholder("");				// Clear the placeholder text
+			setIsCleared(true); 			// Set the cleared state to true
+			gsap.globalTimeline.clear(); 	// Pause all GSAP animations
+		}
 	}, []);
 
+	// Apply the visibility change event listener
+	useEffect(() => {
+		document.addEventListener("visibilitychange", onChangeVisibility);
+		return () => document.removeEventListener("visibilitychange", onChangeVisibility);
+	}, [onChangeVisibility]);
+
+
+	// Animate the placeholder text for the search box
+	useEffect(() => {
+		let intervalId: NodeJS.Timeout;
+		let blinkInterval: NodeJS.Timeout | undefined = undefined;
+		let clearTimeoutId: NodeJS.Timeout | undefined = undefined;
+		
+		let typewriterTween: gsap.core.Tween | undefined = undefined;
+		let blinkTween: gsap.core.Tween | undefined = undefined;
+
+		const animatePlaceholder = () => {
+			const index = Math.floor(Math.random() * placeholders.length);
+			const placeholder = placeholders[index];
+			const totalDuration = placeholder.length * 0.04;
+			const cursor = "\u258d";
+
+			//
+			// Reset before animating
+			//
+
+			setPlaceholder("");
+
+			typewriterTween?.kill();
+			blinkTween?.kill();
+
+			if (blinkInterval) {
+				clearInterval(blinkInterval);
+			}
+
+			// Animate the text
+			placeholder.split("").forEach((char, i) => {
+				typewriterTween = gsap.delayedCall(i * 0.04, () => {
+					// Add the next character and move the cursor
+					setPlaceholder(prev => `${prev}${char}${cursor}`);
+
+					// Clear the cursor after a short delay
+					delay(() => setPlaceholder(prev => prev.replace(cursor, "")), 0.04);
+				});
+			});
+
+			
+			// Start blinking cursor after typing is done
+			blinkTween = gsap.delayedCall(totalDuration, () => {
+				let visible = false;
+				blinkInterval = setInterval(() => {
+					setPlaceholder(_ => visible? `${placeholder} ${cursor}` : placeholder);
+					visible = !visible;
+				}, 200);
+			});
+		};
+
+		// Only animate on mount if not cleared
+		if (!isCleared) {
+			animatePlaceholder();
+			intervalId = setInterval(() => {
+				animatePlaceholder();
+			}, 4000);
+		}
+
+		// Clear all intervals and timeouts on unmount
+		return () => {
+			clearInterval(intervalId);
+
+			if (blinkInterval) {
+				clearInterval(blinkInterval);
+			}
+
+			if (clearTimeoutId) {
+				clearTimeout(clearTimeoutId);
+			}
+
+			gsap.globalTimeline.clear();
+		};
+	}, [isCleared]);
+
+	// Delay placeholder animation after the search box is cleared for visual effect
+	useEffect(() => {
+		if (!isCleared) {
+			return;
+		}
+
+		const timeout = setTimeout(() => {
+			setIsCleared(false);
+		}, 2000);
+
+		return () => clearTimeout(timeout);
+	}, [isCleared]);
+
+	/**
+	 * The search button for the search box
+	 * 
+	 * @returns The search button component 
+	 */
 	const SearchButton = () => (
 		<Button
 			className="relative right-0 sm:right-auto sm:top-0"
 			variant="light"
 			radius="full"
-			startContent={<FaArrowCircleUp size={30}/>}
+			startContent={<FaArrowCircleUp size={30}/>} 
 			onPress={sendQuery}
 			isIconOnly
 		/>
 	);
 
+	/**
+	 * Send a search query to the server and navigate to the results page
+	 */
 	const sendQuery = () => {
 		if (!canSend) {
 			return;
@@ -61,18 +166,18 @@ export default function SearchBox({ setIsSearching }: SearchBoxProps): JSX.Eleme
 		const encodedQuery = encodeURIComponent(query);
 		router.push(`/people/search?query=${encodedQuery}`);
 	}
-	
+
 	return (
 		<Textarea
 			className="flex justify-center items-center w-full sm:w-2/3 lg:w-1/3 mx-auto px-4 sm:px-0"
 			placeholder={placeholder}
 			startContent={<IoSearch size={20}/>}
+
 			endContent={
 				<>
 					<div className="hidden sm:flex md:absolute md:bottom-1 sm:right-0 flex-col sm:flex-row justify-between w-full px-2">
 						<span className="flex gap-x-1 text-xs text-default-500 italic md:mt-4">
 							<p>LinkedIn semantic search, powered by AlfieAI. Data from</p>
-
 							<Link
 								className="underline"
 								href="https://mixrank.com"
@@ -83,10 +188,8 @@ export default function SearchBox({ setIsSearching }: SearchBoxProps): JSX.Eleme
 								MixRank.
 							</Link>
 						</span>
-
 						<SearchButton/>
 					</div>
-
 					<div className="sm:hidden">
 						<SearchButton/>
 					</div>
@@ -96,8 +199,13 @@ export default function SearchBox({ setIsSearching }: SearchBoxProps): JSX.Eleme
 			onValueChange={(value: string) => {
 				setQuery(value);
 				setCanSend(value.length > 0);
-			}}
 
+				if (value.length === 0) {
+					setPlaceholder("");
+					setIsCleared(true);
+				}
+			}}
+			
 			onKeyDown={(event: KeyboardEvent<HTMLInputElement>) => {
 				if (event.key === "Enter") {
 					event.preventDefault();
@@ -105,5 +213,5 @@ export default function SearchBox({ setIsSearching }: SearchBoxProps): JSX.Eleme
 				}
 			}}
 		/>
-	)
+	);
 }
