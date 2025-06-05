d8ee5efb3403d11877dc2e1c039d82464b9194d3
Ryan Smith <rysmith2113@gmail.com>
2025-06-04 23:55:33 -0400

    Call the local embedding model

--- lib/ai.ts
+++ lib/ai.ts
@@ -4,22 +4,22 @@
 // Copyright (c) 2025 Ryan Smith, Adithya Kommi
 //
 
-import fs from "fs";
-import { pipeline } from "@xenova/transformers";
+import { env, pipeline } from "@xenova/transformers";
 
 export async function embed(text: string) {
-	const cachePath = process.env.TRANSFORMERS_CACHE_DIR!;
-
-	// Ensure the directory exists
-	if (!fs.existsSync(cachePath)) {
-		fs.mkdirSync(cachePath, { recursive: true });
-	}
+	// Set up the environment to prefer local models
+	env.localModelPath = "models/";
+	env.allowRemoteModels = false;
 
 	// Load the embedding pipeline with a sentence transformer model
 	const extractor = await pipeline(
 		"feature-extraction",
-		"Xenova/all-MiniLM-L6-v2",
-		{ cache_dir: "/dev/null" }
+		"/sentence-transformers/all-MiniLM-L6-v2/",
+		{
+			cache_dir: "/dev/null",
+			progress_callback: console.log,
+			revision: undefined
+		}
 	);
 
 	// Get embedding for a sentence
