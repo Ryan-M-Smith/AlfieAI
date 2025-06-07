2b4a1cb9bc2bc788a09597408a9d07b6cadec6e2
Ryan Smith <rysmith2113@gmail.com>
2025-06-07 00:11:43 -0400

    Access the embedding model through the microservice

--- lib/ai.ts
+++ lib/ai.ts
@@ -4,30 +4,17 @@
 // Copyright (c) 2025 Ryan Smith, Adithya Kommi
 //
 
-import { env, pipeline } from "@xenova/transformers";
-import path from "path";
-
-export async function embed(text: string) {
-	// Set up the environment to prefer local models
-	env.localModelPath = path.join(process.cwd(), "/models/");
-	env.allowRemoteModels = false;
-
-	// Load the embedding pipeline with a sentence transformer model
-	const extractor = await pipeline(
-		"feature-extraction",
-		"/sentence-transformers/all-MiniLM-L6-v2/",
+export async function embed(text: string): Promise<any[]> {
+	const response = await fetch(
+		process.env.EMBEDDING_MODEL_URL!,
 		{
-			cache_dir: "/dev/null",
-			progress_callback: console.log,
-			revision: undefined
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ text: text }),
 		}
 	);
-
-	// Get embedding for a sentence
-	const output = await extractor(text, {
-		pooling: "mean",
-		normalize: true
-	});
-
-	return Array.from(output.data);
+	const json = await response.json();
+	return json.embeddings;
 }
