6221244413cc617cc330b35d7784ff81992b689f
Ryan Smith <rysmith2113@gmail.com>
2025-06-14 01:32:13 -0400

    Update the Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ import {heroui} from "@heroui/theme"
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
@@ -12,14 +13,14 @@ const config = {
       fontFamily: {
         sans: ["var(--font-sans)"],
         mono: ["var(--font-mono)"],
-      },
+      }
     },
   },
   darkMode: "class",
   plugins: [
     heroui(),
     require("@tailwindcss/typography")
-  ],
+  ]
 }
 
 module.exports = config;
