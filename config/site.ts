28eaead30f648b0216dc213b1de5e5596e06d32f
Ryan Smith <rysmith2113@gmail.com>
2025-06-26 23:31:40 -0400

    Update the site description

--- config/site.ts
+++ config/site.ts
@@ -2,5 +2,5 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
 	name: "AlfieAI",
-	description: "The AI expert on Juniata College. No eagle flies without AI! 🦅",
+	description: "The first-ever all-in-one AI platform for Juniata College. Providing students with relevant information and building their network since April 2025. Juniata LAS 2025 Project."
 };
