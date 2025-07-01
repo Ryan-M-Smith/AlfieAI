90758353cd44b4bd100c351ff521eabc992d1625
Ryan Smith <rysmith2113@gmail.com>
2025-07-01 00:04:50 -0400

    Adjust the back button to work well on mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -10,11 +10,11 @@ import { BsGithub } from "react-icons/bs";
 import { Button } from "@heroui/button";
 import { Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger } from "@heroui/dropdown";
 import Image from "next/image";
-import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowDown } from "react-icons/io";
 import { JSX } from "react";
 import Link from "next/link";   
-import { IoIosArrowDown, IoIosArrowDropleft } from "react-icons/io";
 import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import { getPage, Page, pages } from "@/lib/pages";
 
@@ -22,10 +22,30 @@ export default function Navbar(): JSX.Element {
 	const pathname = usePathname();
 	const router = useRouter();
 
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const checkMobile = () => setIsMobile(window.innerWidth < 640);
+		checkMobile();
+
+		window.addEventListener("resize", checkMobile);
+		return () => window.removeEventListener("resize", checkMobile);
+	}, []);
+
 	const page = getPage(pathname) ?? pages.features.chat;
 	const { features, info } = pages;
 
-	const BackButton = () => (
+	const BackButton = () => isMobile? (
+		<Button
+			className="fixed left-2 top-[7rem] w-fit mx-auto text-default-500 backdrop-blur-lg shadow-lg z-20 pr-1"
+			size={"md"}
+			radius="full"
+			variant="ghost"
+			onPress={ () => router.back() }
+			startContent={<IoIosArrowBack size={30} />}
+			isIconOnly
+		/>
+	) : (
 		<Button
 			className="font-bold text-lg font-mono ml-4"
 			variant="light"
@@ -34,79 +54,81 @@ export default function Navbar(): JSX.Element {
 		>
 			Back
 		</Button>
-	)
-	
+	);
+
 	return (
-		<header className={`
-			sticky top-0 z-30 flex items-center justify-between px-2 py-4 border-b dark:border-zinc-800
-			dark:bg-zinc-950/45 border-slate-300 backdrop-blur-lg bg-transparent
-		`}>
-			<div className="flex items-center gap-x-4 pl-0">
-				<Dropdown disableAnimation>
-					<DropdownTrigger>
-						<Button variant="ghost" endContent={<IoIosArrowDown size={20}/>}>
-							{page.card}
-						</Button>
-					</DropdownTrigger>
+		<>
+			{ pathname.includes("/policies/") && <BackButton/> }
 
-					<DropdownMenu aria-label="Static Actions" disabledKeys={ [page.name] }>
-						<DropdownSection title="Features">
-							{ Object.values(features).map((page: Page) => (
-								<DropdownItem 
-									key={page.name} 
-									href={page.href}
-									description={page.description} 
-									textValue={page.name}
-								>
-									{page.card}
-								</DropdownItem>
-							))}
-						</DropdownSection>
+			<header className={`
+				sticky top-0 z-30 flex items-center justify-between px-2 py-4 border-b dark:border-zinc-800
+				dark:bg-zinc-950/45 border-slate-300 backdrop-blur-lg bg-transparent
+			`}>
+				<div className="flex items-center gap-x-4 pl-1">
+					<Dropdown disableAnimation>
+						<DropdownTrigger>
+							<Button variant="ghost" endContent={<IoIosArrowDown size={20}/>}>
+								{page.card}
+							</Button>
+						</DropdownTrigger>
 
-						<DropdownSection title="Info">
-							{ Object.values(info).map((page: Page) => (
-								<DropdownItem 
-									key={page.name} 
-									href={page.href}
-									description={page.description} 
-									textValue={page.name}
-								>
-									{page.card}
-								</DropdownItem>
-							))}
-						</DropdownSection>
-					</DropdownMenu>
-				</Dropdown>
+						<DropdownMenu aria-label="Static Actions" disabledKeys={ [page.name] }>
+							<DropdownSection title="Features">
+								{ Object.values(features).map((page: Page) => (
+									<DropdownItem 
+										key={page.name} 
+										href={page.href}
+										description={page.description} 
+										textValue={page.name}
+									>
+										{page.card}
+									</DropdownItem>
+								))}
+							</DropdownSection>
 
-				{/* { pathname.includes("/policies/") && <BackButton/> } */}
-			</div>
-			
-			<div className="flex gap-2 pr-0">
-				<Link href="https://github.com/Ryan-M-Smith/AlfieAI" target="_blank" rel="noopener noreferrer">
-					<Button
-						className={`
-							flex items-center justify-center rounded-[10px]
-							text-black bg-white border-black border-2 dark:border-none p-0
-							w-[40px] h-[40px] min-w-[40px] min-h-[40px]
-						`}
-						variant="flat"
-						startContent={ <BsGithub size={30} className="mx-auto" /> }
-						isIconOnly
-					/>
-				</Link>
+							<DropdownSection title="Info">
+								{ Object.values(info).map((page: Page) => (
+									<DropdownItem 
+										key={page.name} 
+										href={page.href}
+										description={page.description} 
+										textValue={page.name}
+									>
+										{page.card}
+									</DropdownItem>
+								))}
+							</DropdownSection>
+						</DropdownMenu>
+					</Dropdown>
+				</div>
+				
+				<div className="flex gap-2 pr-1">
+					<Link href="https://github.com/Ryan-M-Smith/AlfieAI" target="_blank" rel="noopener noreferrer">
+						<Button
+							className={`
+								flex items-center justify-center rounded-[10px]
+								text-black bg-white border-black border-2 dark:border-none p-0
+								w-[40px] h-[40px] min-w-[40px] min-h-[40px]
+							`}
+							variant="flat"
+							startContent={ <BsGithub size={30} className="mx-auto" /> }
+							isIconOnly
+						/>
+					</Link>
 
-				<div className="border-r-1 border-default-400"/>
+					<div className="border-r-1 border-default-400"/>
 
-				<Link href="/">
-					<Image
-						className="inline-block mr-2 rounded-[10px]"
-						src="/logo.png"
-						alt="AlfieAI Logo"
-						width={40}
-						height={40}
-					/>
-				</Link>
-			</div>
-		</header>
+					<Link href="/">
+						<Image
+							className="inline-block mr-2 rounded-[10px]"
+							src="/logo.png"
+							alt="AlfieAI Logo"
+							width={40}
+							height={40}
+						/>
+					</Link>
+				</div>
+			</header>
+		</>
 	);
 }
