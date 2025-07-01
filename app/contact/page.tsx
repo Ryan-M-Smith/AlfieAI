c05c5aba1415475dfa706e0e98466a701465c109
Ryan Smith <rysmith2113@gmail.com>
2025-07-01 00:38:07 -0400

    Remove spacing

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -17,7 +17,7 @@ export default function Contact(): JSX.Element {
 			<div className="flex flex-col justify-center items-center py-10 gap-y-10">
 				<h1 className="flex md:flex-row flex-col justify-center items-center text-5xl sm:text-7xl gap-x-3 sm:gap-x-6">
 					<span className="text-lime-500 font-cursive font-bold">Contact</span>
-					<span> the AlfieAI Team </span>
+					<span>the AlfieAI Team</span>
 				</h1>
 
 				<ContactForm/>
