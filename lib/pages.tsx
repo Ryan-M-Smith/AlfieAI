7485b5f33128db1545a3f81fa6f082154caab071
Ryan Smith <rysmith2113@gmail.com>
2025-08-28 17:21:30 -0400

    Add data for the new live chat page

--- lib/pages.tsx
+++ lib/pages.tsx
@@ -5,6 +5,7 @@
 //
 
 import { BsAsterisk } from "react-icons/bs";
+import { FaMicrophone } from "react-icons/fa";
 import { IoChatboxEllipses } from "react-icons/io5";
 import { MdOutlineContactSupport } from "react-icons/md";
 import { PiGavelFill } from "react-icons/pi";
@@ -46,6 +47,21 @@ export const pages: PageLayout = {
 					<BsAsterisk size={15}/>
 					<span className="text-yellow-500 dark:text-yellow-300 font-serif font-thin">
 						People
+					</span>
+				</span>
+			)
+		},
+
+		live: {
+			href: "/live",
+			name: "live",
+			description: "Experience a live audio chat with AlfieAI",
+			card: (
+				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-bold tracking-tight">
+					AlfieAI
+					<FaMicrophone size={15}/>
+					<span className="text-red-500 font-big font-thin">
+						Live
 					</span>
 				</span>
 			)
