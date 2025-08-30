97e951c3b7ea4ac4b2888065ca431943981d20b8
Ryan Smith <rysmith2113@gmail.com>
2025-08-30 12:03:46 -0400

    Change the font weight on the navbar

--- lib/pages.tsx
+++ lib/pages.tsx
@@ -27,10 +27,10 @@ export const pages: PageLayout = {
 			name: "chat",
 			description: "Chat with AlfieAI",
 			card: (
-				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-bold tracking-tight">
+				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-medium tracking-tight">
 					AlfieAI
 					<IoChatboxEllipses size={15}/>
-					<span className="text-primary">
+					<span className="text-primary font-bold">
 						Chat
 					</span>
 				</span>
@@ -42,7 +42,7 @@ export const pages: PageLayout = {
 			name: "people",
 			description: "Search Juniata's network of students and professionals",
 			card: (
-				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-bold tracking-tight">
+				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-medium tracking-tight">
 					AlfieAI
 					<BsAsterisk size={15}/>
 					<span className="text-yellow-500 dark:text-yellow-300 font-serif font-thin">
@@ -57,7 +57,7 @@ export const pages: PageLayout = {
 			name: "live",
 			description: "Experience a live audio chat with AlfieAI",
 			card: (
-				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-bold tracking-tight">
+				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-medium tracking-tight">
 					AlfieAI
 					<FaMicrophone size={15}/>
 					<span className="text-red-500 font-big font-thin">
@@ -74,7 +74,7 @@ export const pages: PageLayout = {
 			name: "policies",
 			description: "The stuff we have to write and you don't want to read",
 			card: (
-				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-bold tracking-tight">
+				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-medium tracking-tight">
 					AlfieAI
 					<PiGavelFill size={15}/>
 					<span className="text-orange-500 font-light font-mono">
@@ -89,7 +89,7 @@ export const pages: PageLayout = {
 			name: "contact",
 			description: "Contact the AlfieAI team",
 			card: (
-				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-bold tracking-tight">
+				<span className="flex flex-row justify-center items-center gap-1 text-2xl font-medium tracking-tight">
 					AlfieAI
 					<MdOutlineContactSupport size={15}/>
 					<span className="text-lime-500 font-bold font-cursive">
