33a20af8bfa54759dece6ef5c67b7918f91dbd8c
Ryan Smith <rysmith2113@gmail.com>
2025-08-30 23:38:12 -0400

    Update boilerplate

--- components/chat-view.tsx
+++ components/chat-view.tsx
@@ -1,11 +1,7 @@
 //
 // Filename: chat-view.tsx
 // Description: The chatbot interface
-<<<<<<< HEAD
-// Copyright (c) 2025 Ryan Smith
-=======
 // Copyright (c) 2025 Ryan Smith <rysmith2113@gmail.com>
->>>>>>> 19dbd4e (Update boilerplate)
 //
 
 "use client";
