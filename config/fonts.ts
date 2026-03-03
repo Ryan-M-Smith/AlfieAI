207c48f38ea8cc100621c9a4230f0b258a986880
Ryan Smith <rysmith2113@gmail.com>
2026-03-03 01:25:34 -0500

    Add the new font

--- config/fonts.ts
+++ config/fonts.ts
@@ -3,7 +3,8 @@ import {
 	Inter as FontSans,
 	Dancing_Script as FontCursive,
 	Black_Ops_One as FontBig,
-	Modak as FontBubble
+	Modak as FontBubble,
+	Racing_Sans_One as FontRacing
 } from "next/font/google";
 
 export const fontSans = FontSans({
@@ -25,10 +26,16 @@ export const fontBig = FontBig({
 	subsets: ["latin"],
 	weight: "400",
 	variable: "--font-big"
-})
+});
 
 export const fontBubble = FontBubble({
 	subsets: ["latin"],
 	weight: "400",
 	variable: "--font-bubble"
-})
+});
+
+export const fontRacing = FontRacing({
+	subsets: ["latin"],
+	weight: "400",
+	variable: "--font-racing"
+});
